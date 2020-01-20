d8d2dd909a3586f69689972cbf31954efe6729d9
Siva Krishna <shivataraya@gmail.com>
2020-01-20 17:06:33 -0600

    add resultsshowscreen for individual entity

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity
+} from "react-native";
+import { withNavigation } from "react-navigation";
 import ResultsDetail from "./ResultsDetail";
 
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
+  if (!results.length) {
+    return null;
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
@@ -12,7 +22,15 @@ const ResultsList = ({ title, results }) => {
         keyExtractor={result => result.id}
         data={results}
         renderItem={({ item }) => {
-          return <ResultsDetail result={item} />;
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("ResultsShow", { id: item.id })
+              }
+            >
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          );
         }}
         sc
       />
@@ -32,4 +50,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
