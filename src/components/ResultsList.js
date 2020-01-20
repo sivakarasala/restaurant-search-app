29691eb6f3c4cfacf20229b983b46912c41b2ee0
Siva Krishna <shivataraya@gmail.com>
2020-01-20 16:16:02 -0600

    add results list comp to render grouped results

---
+++ src/components/ResultsList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const ResultsList = ({ title, results }) => {
+  return (
+    <View>
+      <Text style={styles.title}>{title}</Text>
+      <FlatList
+        horizontal
+        keyExtractor={result => result.id}
+        data={results}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>;
+        }}
+        sc
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: "bold"
+  }
+});
+
+export default ResultsList;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
 
+  const filterResultsByPrice = price => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter(result => result.price === price);
+  };
   return (
     <View>
       <SearchBar
@@ -17,6 +21,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice("$")} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice("$$")} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice("$$$")} title="Big Spender" />
     </View>
   );
 };
