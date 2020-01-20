98134f7a5c11a8e80beb167f47b54a7e7edcc9bb
Siva Krishna <shivataraya@gmail.com>
2020-01-20 15:52:44 -0600

    handle yelp api call

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async searchTerm => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "st louis"
+        }
+      });
+      setResults(response.data.businesses);
+      setErrorMessage("");
+    } catch (err) {
+      setErrorMessage("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    searchApi("dosa");
+  }, [term]);
 
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log("term was submitted")}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text>{term}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
