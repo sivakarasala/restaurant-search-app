7882c8d20c1dcafa690a0b47ff5b907809e97b81
Siva Krishna <shivataraya@gmail.com>
2020-01-20 15:00:04 -0600

    setup init stacknavigation

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Aum Namah Shivaya</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
